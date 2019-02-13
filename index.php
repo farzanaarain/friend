20f8286bd5d06ab1c79396ce746625f482a0a88c
farzanaarain <Farzana.bunglow@gmail.com>
2019-02-13 10:54:46 -0800

    added index file

---
+++ index.php
@@ -0,0 +1,11 @@
+<form action= "" method= "get">
+	enter name : <input type= "text" name= "frind_name"/>
+	 <input type= "submit" value= "add frind"/>
+	 
+	
+</form>
+
+<?php
+     $f_name= $_GET['frind_name'];
+	 echo "$f_name";
+	 ?>
